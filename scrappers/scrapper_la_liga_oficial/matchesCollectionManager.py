0da5d10172e56f4f23d90067ee12bb6a4444773f
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2016-06-28 11:05:50 +0200

    Do I really need reports generator when I can use logger?

--- scrappers/scrapper_la_liga_oficial/matchesCollectionManager.py
+++ scrappers/scrapper_la_liga_oficial/matchesCollectionManager.py
@@ -1,10 +1,7 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from reportsGenerator import ReportsGenerator
 
 class MatchesCollectionManager:
-	def __init__(self):
-		self.reportsGenerator = ReportsGenerator()
 
 	def find_a_match(self, player1, player2, date):
 		db = MongoClient('localhost',3001).meteor
@@ -18,11 +15,9 @@ class MatchesCollectionManager:
 	def insert_a_new_match(self, newMatch):
 		db = MongoClient('localhost',3001).meteor
 		matchesCollection = db.matches
-		self.reportsGenerator.generate_insert_match_record(newMatch);
 		return matchesCollection.insert(newMatch)
 
 	def update_an_existing_match(self, match):
 		db = MongoClient('localhost',3001).meteor
 		matchesCollection = db.matches
-		self.reportsGenerator.generate_updated_match_record(match);
 		return matchesCollection.update({'_id' : match['_id']}, {"$set" : match})
