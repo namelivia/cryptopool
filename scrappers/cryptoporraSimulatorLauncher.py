610f9cd18a2a83d69c74569f4168ab5f35c03fc9
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2016-11-24 20:04:47 +0100

    Interactive mode for the simulator

--- scrappers/cryptoporraSimulatorLauncher.py
+++ scrappers/cryptoporraSimulatorLauncher.py
@@ -1,7 +1,54 @@
 from cryptoporraSimulator import CryptoporraSimulator
 def main():
 	cryptoporraSimulator = CryptoporraSimulator() 
-	while True :
-		cryptoporraSimulator.simulation_step()
+	opt = True
+	while opt:
+		print("""
+			[Cryptoporra Simulator]
+			1.- Interactive mode
+			2.- Auto mode
+			3.- Exit
+		""")
+		opt = raw_input("What mode do you want to run? ")
+		if opt=="1":
+			print("Interactive mode")
+			intOpt = True
+			while intOpt:
+				print("""
+			[Interactive mode]
+			1.- Create a random match
+			2.- Update a random match
+			3.- Make a random bet
+			4.- Make a random pool
+			5.- Make a random user
+			6.- Go back
+				""")
+				intOpt = raw_input("What operation do you want to do? ")
+				if intOpt=="1":
+					cryptoporraSimulator.create_a_random_match()
+					print("Random match created")
+				elif intOpt=="2":
+					cryptoporraSimulator.random_update_a_match()
+					print("Random match updated")
+				elif intOpt=="3":
+					cryptoporraSimulator.make_a_random_bet()
+					print("Random bet created")
+				elif intOpt=="4":
+					cryptoporraSimulator.make_a_random_pool()
+					print("Random pool created")
+				elif intOpt=="5":
+					cryptoporraSimulator.make_a_random_user()
+					print("Random user created")
+				elif intOpt=="6":
+					intOpt = False
+		elif opt=="2":
+			print("Auto mode")
+		elif opt=="3":
+			print("Goodbye")
+			exit()
+		elif opt!="":
+			print("Invalid option, try again")
+	#while True :
+	#	cryptoporraSimulator.simulation_step()
 if __name__ == "__main__":
 	main()
